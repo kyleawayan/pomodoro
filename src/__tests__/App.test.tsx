102a2fd2def8fd35a8feb4e0097c276cb041687b
Kyle Awayan <kyle@awayan.com>
2023-07-10 18:22:38 -0700

    mock ipcRenderer

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,20 @@ import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
 describe('App', () => {
+  // create a fake ipcRenderer
+  const fakeIpcRenderer = {
+    sendMessage: jest.fn(),
+    on: jest.fn(),
+    once: jest.fn(),
+  };
+
+  // Mock the window.electron.ipcRenderer
+  beforeAll(() => {
+    window.electron = {
+      ipcRenderer: fakeIpcRenderer,
+    };
+  });
+
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
   });

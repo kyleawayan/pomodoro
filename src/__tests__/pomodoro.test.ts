8305669b4e9ea9bee289161b4cdd3371ae96ab2a
Kyle Awayan <kyle@awayan.com>
2023-07-10 21:18:33 -0700

    simplify tests, add some tests after long break

--- src/__tests__/pomodoro.test.ts
+++ src/__tests__/pomodoro.test.ts
@@ -1,77 +1,119 @@
 import Pomodoro from 'main/pomodoro';
 
+function getPomodoroResults(startTime: Date, fakeTime: Date) {
+  const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
+  pomodoro.calculatePomodoro(startTime, fakeTime);
+  return pomodoro.info;
+}
+
 describe('Pomodoro class', () => {
   describe('calculatePomodoro method', () => {
     // Here is an example pomodoro session that starts at 8:00PM
     const startTime = new Date(2023, 7, 11, 20, 0, 0); // 8:00PM
 
     it('correctly recognizes first work interval', () => {
-      const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
-
       // Work session starts at 8:00PM and ends at 8:25PM
-      const testTime = new Date(2023, 7, 11, 20, 10, 0); // 8:10PM
+      // Test 8:10PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 20, 10, 0)
+      );
 
-      pomodoro.calculatePomodoro(startTime, testTime);
-      expect(pomodoro.info.currentEvent.type).toEqual('pomodoro');
-      expect(pomodoro.info.currentEvent.endTime).toEqual(
+      expect(res.currentEvent.type).toEqual('pomodoro');
+      expect(res.currentEvent.endTime).toEqual(
         new Date(2023, 7, 11, 20, 25, 0) // Should end at 8:25PM
       );
-      expect(pomodoro.info.pomodoroCount).toEqual(1);
+      expect(res.pomodoroCount).toEqual(1);
     });
 
     it('correctly recognizes first short break interval', () => {
-      const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
-
       // Short break session starts at 8:25PM and ends at 8:30PM
-      const testTime = new Date(2023, 7, 11, 20, 27, 0); // 8:27PM
+      // Test 8:27PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 20, 27, 0)
+      );
 
-      pomodoro.calculatePomodoro(startTime, testTime);
-      expect(pomodoro.info.currentEvent.type).toEqual('shortBreak');
-      expect(pomodoro.info.currentEvent.endTime).toEqual(
+      expect(res.currentEvent.type).toEqual('shortBreak');
+      expect(res.currentEvent.endTime).toEqual(
         new Date(2023, 7, 11, 20, 30, 0) // Should end at 8:30PM
       );
-      expect(pomodoro.info.pomodoroCount).toEqual(1);
+      expect(res.pomodoroCount).toEqual(1);
     });
 
     it('correctly recognizes second work interval', () => {
-      const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
-
       // Work session starts at 8:30PM and ends at 8:55PM
-      const testTime = new Date(2023, 7, 11, 20, 46, 0); // 8:46PM
+      // Test 8:46PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 20, 46, 0)
+      );
 
-      pomodoro.calculatePomodoro(startTime, testTime);
-      expect(pomodoro.info.currentEvent.type).toEqual('pomodoro');
-      expect(pomodoro.info.currentEvent.endTime).toEqual(
+      expect(res.currentEvent.type).toEqual('pomodoro');
+      expect(res.currentEvent.endTime).toEqual(
         new Date(2023, 7, 11, 20, 55, 0) // Should end at 8:55PM
       );
-      expect(pomodoro.info.pomodoroCount).toEqual(2);
+      expect(res.pomodoroCount).toEqual(2);
     });
 
     it('correctly recognizes second short break interval', () => {
-      const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
-
       // Short break session starts at 8:55PM and ends at 9:00PM
-      const testTime = new Date(2023, 7, 11, 20, 58, 0); // 8:58PM
+      // Test 8:58PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 20, 58, 0)
+      );
 
-      pomodoro.calculatePomodoro(startTime, testTime);
-      expect(pomodoro.info.currentEvent.type).toEqual('shortBreak');
-      expect(pomodoro.info.currentEvent.endTime).toEqual(
+      expect(res.currentEvent.type).toEqual('shortBreak');
+      expect(res.currentEvent.endTime).toEqual(
         new Date(2023, 7, 11, 21, 0, 0) // Should end at 9:00PM
       );
-      expect(pomodoro.info.pomodoroCount).toEqual(2);
+      expect(res.pomodoroCount).toEqual(2);
     });
 
     it('correctly recognizes long break interval', () => {
-      const pomodoro = new Pomodoro(() => Promise.resolve(startTime));
-
       // Long break session starts at 9:55PM and ends at 10:15PM
-      const testTime = new Date(2023, 7, 11, 21, 55, 0); // 9:55PM
+      // Test 9:55PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 21, 55, 0)
+      );
 
-      pomodoro.calculatePomodoro(startTime, testTime);
-      expect(pomodoro.info.currentEvent.type).toEqual('longBreak');
-      expect(pomodoro.info.currentEvent.endTime).toEqual(
+      expect(res.currentEvent.type).toEqual('longBreak');
+      expect(res.currentEvent.endTime).toEqual(
         new Date(2023, 7, 11, 22, 15, 0) // Should end at 10:15PM
       );
+      expect(res.pomodoroCount).toEqual(4);
+    });
+
+    it('correctly recognizes work interval after long break', () => {
+      // Work session after long break starts at 10:15PM and ends at 10:40PM
+      // Test 10:30PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 22, 30, 0)
+      );
+
+      expect(res.currentEvent.type).toEqual('pomodoro');
+      expect(res.currentEvent.endTime).toEqual(
+        new Date(2023, 7, 11, 22, 40, 0) // Should end at 10:40PM
+      );
+      expect(res.pomodoroCount).toEqual(5);
+    });
+
+    it('correctly recognizes first short break interval after long break', () => {
+      // The first short break session after long break starts at 10:40PM and ends at 10:45PM
+      // Test 10:42PM
+      const res = getPomodoroResults(
+        startTime,
+        new Date(2023, 7, 11, 22, 42, 0)
+      );
+
+      expect(res.currentEvent.type).toEqual('shortBreak');
+      expect(res.currentEvent.endTime).toEqual(
+        new Date(2023, 7, 11, 22, 45, 0) // Should end at 10:45PM
+      );
+      expect(res.pomodoroCount).toEqual(5);
     });
   });
 });
